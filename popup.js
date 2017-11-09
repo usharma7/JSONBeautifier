6e9df8d1b4c36e495511f80083724420c0a6ed02
Utkarsh Sharma <utkarsh7sharma@gmail.com>
2017-11-09 00:35:20 -0800

    Button bug fixed, JSON parsing

--- popup.js
+++ popup.js
@@ -1,10 +1,17 @@
+document.getElementById('button').addEventListener('click', getJSON);
+
 function getJSON(){
   //fetch JSON from textbox and beautify
-  var t = document.getElementById('textArea').getvalue;
-  // var beautified = JSON.parse(t)
-  document.getElementById('textArea').value = t + "Clicked!";
-      console.log("Clicked!")
+  try{
+  var t = document.getElementById('textArea').value;
+  var beautified = JSON.parse(t)
+  beautified= JSON.stringify(beautified, null, 4)
+  document.getElementById('button').value = beautified;
+
+
+    }
+    catch (Exception ex){
+
+    }
 
 }
-
-document.getElementById('button').addEventListener('click', getJSON());
